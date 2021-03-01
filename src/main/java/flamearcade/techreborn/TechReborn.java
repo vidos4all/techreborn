fb940eea96d10f966d166c37e96ebd92193162ad
vidos4all <vidos4all@gmail.com>
2021-03-01 12:20:21 +0200

    Creative Tab | Added

--- src/main/java/flamearcade/techreborn/TechReborn.java
+++ src/main/java/flamearcade/techreborn/TechReborn.java
@@ -2,6 +2,10 @@ package flamearcade.techreborn;
 
 import flamearcade.techreborn.proxy.CommonProxy;
 import flamearcade.techreborn.utils.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -13,6 +17,20 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class TechReborn {
 	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
 	public static CommonProxy proxy;
+	
+	
+	public static final CreativeTabs CTAB = new CreativeTabs("trtab") { 
+	    @Override
+	    public ItemStack getTabIconItem() // Set the creative tab icon as a furnace(TEMPORARY)
+	    {
+	        return new ItemStack(Item.getItemFromBlock(Blocks.FURNACE));
+	    }
+	    
+	    @Override
+		public boolean hasSearchBar() { // Enabled the search bar for a tab
+			return true;
+		}
+	}.setBackgroundImageName("item_search.png");
 	
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent event)
