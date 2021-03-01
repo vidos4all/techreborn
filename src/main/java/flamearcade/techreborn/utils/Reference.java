e61df256e3f5e7e3a9e3baa70e2f6ec0e76f9082
vidos4all <vidos4all@gmail.com>
2021-03-01 11:45:53 +0200

    Reference file | Created

---
+++ src/main/java/flamearcade/techreborn/utils/Reference.java
@@ -0,0 +1,7 @@
+package flamearcade.techreborn.utils;
+
+public class Reference {
+	public static final String MODID = "techreborn";
+	public static final String CLIENT = "flamearcade.techreborn.proxy.ClientProxy";
+	public static final String COMMON = "flamearcade.techreborn.proxy.CommonProxy";
+}
